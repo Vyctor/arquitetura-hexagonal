e191f6913977554a5d72daff1644d4bc30830d81
vyctorvguimaraes <vyctor.guimaraes@conexia.com.br>
2022-06-20 19:15:13 -0300

    trabalhando com gomock e mockgen

--- application/product_service_test.go
+++ application/product_service_test.go
@@ -1,1 +1,27 @@
 package application_test
+
+import (
+	"testing"
+
+	"github.com/codeedu/go-hexagonal/application"
+	mock_application "github.com/codeedu/go-hexagonal/application/mocks"
+	"github.com/golang/mock/gomock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestProductService_Get(test *testing.T) {
+	controller := gomock.NewController(test)
+	defer controller.Finish()
+
+	product := mock_application.NewMockProductInterface(controller)
+	persistence := mock_application.NewMockProductPersistenceInterface(controller)
+	persistence.EXPECT().Get(gomock.Any()).Return(product, nil)
+
+	service := application.ProductService{
+		Persistence: persistence,
+	}
+
+	result, err := service.Get("abc")
+	require.Nil(test, err)
+	require.Equal(test, product, result)
+}
